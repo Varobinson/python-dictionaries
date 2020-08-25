6cfe0f532bc46cc0581df5f53ad485a8fc9f7244
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-08-25 16:56:19 -0400

    dictionary letter summary

--- letter_sum.py
+++ letter_sum.py
@@ -3,13 +3,12 @@ word = input('Pick a word any word!! \n>').lower()
 def letter_histogram(inp):
     new_dict = {}
     for letters in inp:
-        if letters in new_dict:
+        if letters in new_dict.keys():
           new_dict[letters] += 1
         else:
-          new_dict[letters] = 1
-    
+          new_dict[letters] = 1    
     for key in new_dict:
-        if new_dict[key] >= 1:
+        if new_dict[key] >= True:
             print({f'{key} : {new_dict[key]}'})
             
         
